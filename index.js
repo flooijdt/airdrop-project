6a6b38e8762a21aa0d09a18004002bc313197f35
flooijdt <flooijdt@localhost.localdomain>
2022-12-08 10:37:26 -0300

    first

--- index.js
+++ index.js
@@ -26,12 +26,15 @@ const airDropSol = async () => {
   try {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
     const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+    await connection.confirmTransaction(fromAirDropSignature)
   } catch (err) {
     console.error(err)
   }
 }
 
 const main = async () => {
+  await getWalletBalance()
+  await airDropSol()
   await getWalletBalance()
 }
 
