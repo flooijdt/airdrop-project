5f75f5ab72c96406e952e01d8cbd6df23101d9f8
flooijdt <flooijdt@localhost.localdomain>
2022-12-08 11:02:31 -0300

    first

--- index.js
+++ index.js
@@ -25,14 +25,14 @@ const getWalletBalance = async () => {
 const airDropSol = async () => {
   try {
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
-    const fromAirDropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
+    const airdropSignature = await connection.requestAirdrop(publicKey, 2 * LAMPORTS_PER_SOL)
 
     const latestBlockHash = await connection.getLatestBlockhash();
 
     await connection.confirmTransaction({
       blockhash: latestBlockHash.blockhash,
       lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: fromAirDropSignature,
+      signature: airdropSignature,
     });
   } catch (err) {
     console.log(err)
